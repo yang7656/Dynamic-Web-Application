cb63137edb6dec217222e2c698795a55657499fe
Shijun Yang <55370158+yang7656@users.noreply.github.com>
2019-12-10 10:15:03 -0600

    luca

--- script/scripts.js
+++ script/scripts.js
@@ -1,45 +1,136 @@
 // Luca and Shijun
-
+// Scripts for Vue and Leaflet
 var app;
+var lat =44.949642;
+var lng =-93.093124;
+var mymap;
+var latlng;
+var addressLat;
+var addressLng;
 
 function Init() {
-	
-	var map = L.map('map').setView([44.954445, -93.091301], 11);
-	L.tileLayer("https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=IBQRTgg4PbevBpCDSJFF", {
-		attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
-		maxZoom: 18,
-		minZoom: 11
-	}).addTo(map);
-	
-	var marker = L.marker([44.954445, -93.091301]).addTo(map);
-	
-	document.getElementsByName('search')[0].placeholder = new L.LatLng(44.954445, -93.091301);
-	
-	map.on('click', onMapClick => {
-		marker.setLatLng(onMapClick.latlng);
-		document.getElementsByName('search')[0].placeholder = onMapClick.latlng;
-	});
-	
-	map.on("moveend", function() {
-	  document.getElementsByName('search')[0].placeholder = map.getCenter().toString();
-	});
-	
-	console.log(document.getElementById("search").values);
-	
-	
-	
-	
-	
-	
-	
-	var app = new Vue({
+	// VUE
+	app = new Vue({
 		el: '#app',
 		data: {
-	    	message: 'Hello Vue!'
-		}
+			mapDiv: false,
+			tableDiv: false,
+			addressDiv: true,
+			userLat: "",
+			userLng: "",
+			inputSearch: "",
+            searchType: "select",
+            searchTypeOptions: [
+            	{ value: "select", text: "select" },
+                { value: "address", text: "address" },
+                { value: "latlong", text: "latlong" }
+            ],
+            stpaulcrimes: []
+        },
+        methods:{
+	        showMap: function(){
+	            this.mapDiv = !this.mapDiv;
+	        },
+	        showTable: function() {
+	        	this.tableDiv = !this.tableDiv;
+	        },
+	        changeAddressDiv: function(value){
+	        	if (this.searchType === 'latlong') {
+	        		this.addressDiv = false;
+	        	} else {
+	        		this.addressDiv = true;
+	        	}
+	        }
+    	}
 	});
+
+	//MAP
+	latlng = L.latLng(lat, lng);
+	mymap = L.map('mapid', {
+		center: latlng,
+		zoom: 12
+	});/*.setView(latlng, 15);*/
+	mymap.setMinZoom(11);
+	mymap.setMaxZoom(18);
+    var southWest = L.latLng(44.949642-0.06, -93.093124+0.18);
+    var northEast = L.latLng(44.949642+0.06, -93.093124-0.18);
+    var mybounds = L.latLngBounds(southWest, northEast);
+	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	    bounds: mybounds,
+	    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	}).addTo(mymap);
+    
+    // marker
+    var marker = L.marker([44.954445, -93.091301]).addTo(mymap);
+	// latlng in box
+	document.getElementsByName('box')[0].placeholder = new L.LatLng(44.954445, -93.091301);
+	// latlng in box after click
+	mymap.on('click', onMapClick => {
+		marker.setLatLng(onMapClick.latlng);
+		document.getElementsByName('box')[0].placeholder = onMapClick.latlng;
+	});
+	// latlng in box after pan
+	mymap.on("moveend", function() {
+	  document.getElementsByName('box')[0].placeholder = mymap.getCenter().toString();
+	});
+    
+	//DATA FOR TABLE
+	let request = {
+        url:'http://localhost:8000/incidents?start_date=2019-10-01&end_date=2019-10-31',
+        dataType: "json",
+        success: this.stpaulcrimes
+	};
+
+    $.ajax(request);
 }
 
-function onMapClick(e) {
-	return e.latlng;
+function Search(event) {
+	/*
+	QUERY : https://nominatim.openstreetmap.org/search?<params>
+		street=<housenumber> <streetname>
+		city=<city>
+		county=<county>
+		state=<state>
+		country=<country>
+		postalcode=<postalcode>
+	*/
+	var queryNominatim = 'https://nominatim.openstreetmap.org/search?';
+
+	if (app.searchType == 'select' || app.searchType == 'address') {
+		var street = app.inputSearch.split(' ');
+		var streetName;
+		for (var i = 0; i < street.length; i++) {
+			if (i == 0) {
+				streetName = street[0];
+			} else {
+				streetName = streetName + '%20' + street[i];
+			}
+		}
+
+		let request = {
+	        url: queryNominatim + 'street='+ streetName +'&city=St%20Paul&state=MN&format=json',
+	        dataType: "json",
+	        success: setNominatim
+	    };
+
+	    $.ajax(request).then(() => {
+	    	latlng = L.latLng(addressLat, addressLng);
+			mymap.flyTo(latlng, 18);
+	    });
+	}
+
+	if (app.searchType == 'latlong') {
+		if ((app.userLat>=44.949642-0.06 && app.userLat<=44.949642+0.06) 
+			&& (app.userLng>=-93.093124-0.18  && app.userLng<=-93.093124+0.18)) {
+			latlng = L.latLng(app.userLat, app.userLng);
+			mymap.flyTo(latlng, 18);
+		}
+	}
+	
 }
+
+function setNominatim(data){
+	addressLat = data[0].lat;
+	addressLng = data[0].lon;
+}
+
