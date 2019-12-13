262db642ef702adc1c1719dd60448179ffca62fc
Shijun Yang <55370158+yang7656@users.noreply.github.com>
2019-12-13 13:35:03 -0600

    not final of final

--- script/scripts.js
+++ script/scripts.js
@@ -7,8 +7,11 @@ var mymap;
 var latlng;
 var addressLat;
 var addressLng;
+var currentAddress;
 
-function Init() {
+//function Init(crime_api_url) {
+function Init(crime_api_url) {
+    console.log(crime_api_url);
 	// VUE
 	app = new Vue({
 		el: '#app',
@@ -27,7 +30,15 @@ function Init() {
             ],
             stpaulcrimes: [],
             Codes: [],
-            Neighborhoods: []
+            Neighborhoods: [],
+            currentCoordinate: [],
+            searchStartDate: "",
+            searchEndDate: "",
+            searchStartTime: "",
+            searchEndTime: "",
+            crimeSelected: [],
+            neighSelected: [],
+            unchangeCrime: []
         },
         methods:{
 	        showMap: function(){
@@ -61,91 +72,149 @@ function Init() {
 	    bounds: mybounds,
 	    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 	}).addTo(mymap);
-	mymap.setMaxBounds(mymap.getBounds());
+	mymap.setMaxBounds(mybounds);//mymap.getBounds());
     
-    /*
-	// marker
-    var marker = L.marker([44.954445, -93.091301]).addTo(mymap);
-	// latlng in box
-	document.getElementsByName('box')[0].placeholder = new L.LatLng(44.954445, -93.091301);
-	// latlng in box after click
-	mymap.on('click', onMapClick => {
-		marker.setLatLng(onMapClick.latlng);
-		document.getElementsByName('box')[0].placeholder = onMapClick.latlng;
-	});
-    */
-    
-	// latlng in box after pan
-	mymap.on("moveend", function() {
-        console.log(mymap.getBounds());
-        document.getElementsByName('box')[0].placeholder = mymap.getCenter().toString();
-        
-        
-	});
-    
-    // 17 neighborhoods
-    // 1|Conway/Battlecreek/Highwood; southWest = L.latLng(44.951419, -93.025237); northEast = L.latLng(44.963058, -93.004910); 44.956758, -93.015139
-    L.marker([44.956758, -93.015139]).addTo(mymap);
-    // 2|Greater East Side; southWest = L.latLng(44.963233, -93.045952); northEast = L.latLng(44.991986, -93.005088); 44.977519, -93.025290
-    L.marker([44.977519, -93.025290]).addTo(mymap);
-    // 3|West Side; southWest = L.latLng(44.919771, -93.128445); northEast = L.latLng(44.942021, -93.057143); 44.931369, -93.082249
-    L.marker([44.931369, -93.082249]).addTo(mymap);
-    // 4|Dayton's Bluff; 44.957164, -93.057100
-    L.marker([44.957164, -93.057100]).addTo(mymap);
-    // 5|Payne/Phalen; 44.978208, -93.069673
-    L.marker([44.978208, -93.069673]).addTo(mymap);
-    // 6|North End; 44.977405, -93.110969
-    L.marker([44.977405, -93.110969]).addTo(mymap);
-    // 7|Thomas/Dale(Frogtown); 44.960265, -93.118686
-    L.marker([44.960265, -93.118686]).addTo(mymap);
-    // 8|Summit/University; 44.948581, -93.128205
-    L.marker([44.948581, -93.128205]).addTo(mymap);
-    // 9|West Seventh; 44.931735, -93.119224
-    L.marker([44.931735, -93.119224]).addTo(mymap);
-    // 10|Como; 44.982860, -93.150844
-    L.marker([44.982860, -93.150844]).addTo(mymap);
-    // 11|Hamline/Midway; 44.962891, -93.167436
-    L.marker([44.962891, -93.167436]).addTo(mymap);
-    // 12|St. Anthony; 44.973546, -93.195991
-    L.marker([44.973546, -93.195991]).addTo(mymap);
-    // 13|Union Park; 44.948401, -93.174050
-    L.marker([44.948401, -93.174050]).addTo(mymap);
-    // 14|Macalester-Groveland; 44.934301, -93.175363
-    L.marker([44.934301, -93.175363]).addTo(mymap);
-    // 15|Highland; 44.911489, -93.172075
-    L.marker([44.911489, -93.172075]).addTo(mymap);
-    // 16|Summit Hill; 44.937493, -93.136353
-    L.marker([44.937493, -93.136353]).addTo(mymap);
-    // 17|Capitol River; 44.950459, -93.096462
-    L.marker([44.950459, -93.096462]).addTo(mymap);
     
 	//DATA FOR TABLE
-    $.getJSON('http://localhost:8000/incidents?start_date=2019-10-01&end_date=2019-10-31', (data)=> {
-    	app.stpaulcrimes = data;
+    
+    //$.getJSON('http://cisc-dean.stthomas.edu:8036/incidents', (data)=> {
+    $.getJSON(crime_api_url+'/incidents', (allData)=> {
+        unchangeCrime = allData;
     });
-    $.getJSON('http://localhost:8000/codes', (codes)=> {
+    
+    //$.getJSON('http://cisc-dean.stthomas.edu:8036/incidents?start_date=2019-10-01&end_date=2019-10-31', (data)=> {
+    $.getJSON(crime_api_url + '/incidents?start_date=2019-10-01&end_date=2019-10-31', (data)=> {
+        
+        app.stpaulcrimes = data;
+
+        var commited = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+        var centerOfNeighbor = [];
+        var tem = {};
+        
+        for (var key in data) {
+            commited[data[key].neighborhood_number-1]++;
+            
+            if (data[key].incident.includes("Homicide") || data[key].incident.includes("Rape") || data[key].incident.includes("Assault") || data[key].incident.includes("Asasult") || data[key].incident.includes("Discharge")) {
+                app.stpaulcrimes[key].color = '#d9d366';
+            }
+            else if (data[key].incident.includes("Robbery") || data[key].incident.includes("Theft") || data[key].incident.includes("Burglary") || data[key].incident.includes("Arson") || data[key].incident.includes("Vandalism") || data[key].incident.includes("Graffiti") || data[key].incident.includes("Narcotics")) {
+                app.stpaulcrimes[key].color = '#d6a7a7';
+            }
+            else if (data[key].incident.includes("Police") || data[key].incident.includes("Community")) {
+                app.stpaulcrimes[key].color = '#66a9d9';
+            }
+        }
+        
+        // 17 neighborhoods
+        // 1|Conway/Battlecreek/Highwood; 44.956758, -93.015139
+        centerOfNeighbor.push([44.956758, -93.015139]);
+        L.marker([44.956758, -93.015139]).addTo(mymap).bindPopup(commited[0]+' crimes commited').openPopup();
+        // 2|Greater East Side; 44.977519, -93.025290
+        centerOfNeighbor.push([44.977519, -93.025290]);
+        L.marker([44.977519, -93.025290]).addTo(mymap).bindPopup(commited[1]+' crimes commited').openPopup();
+        // 3|West Side; 44.931369, -93.082249
+        centerOfNeighbor.push([44.931369, -93.082249]);
+        L.marker([44.931369, -93.082249]).addTo(mymap).bindPopup(commited[2]+' crimes commited').openPopup();
+        // 4|Dayton's Bluff; 44.957164, -93.057100
+        centerOfNeighbor.push([44.957164, -93.057100]);
+        L.marker([44.957164, -93.057100]).addTo(mymap).bindPopup(commited[3]+' crimes commited').openPopup();
+        // 5|Payne/Phalen; 44.978208, -93.069673
+        centerOfNeighbor.push([44.978208, -93.069673]);
+        L.marker([44.978208, -93.069673]).addTo(mymap).bindPopup(commited[4]+' crimes commited').openPopup();
+        // 6|North End; 44.977405, -93.110969
+        centerOfNeighbor.push([44.977405, -93.110969]);
+        L.marker([44.977405, -93.110969]).addTo(mymap).bindPopup(commited[5]+' crimes commited').openPopup();
+        // 7|Thomas/Dale(Frogtown); 44.960265, -93.118686
+        centerOfNeighbor.push([44.960265, -93.118686]);
+        L.marker([44.960265, -93.118686]).addTo(mymap).bindPopup(commited[6]+' crimes commited').openPopup();
+        // 8|Summit/University; 44.948581, -93.128205
+        centerOfNeighbor.push([44.948581, -93.128205]);
+        L.marker([44.948581, -93.128205]).addTo(mymap).bindPopup(commited[7]+' crimes commited').openPopup();
+        // 9|West Seventh; 44.931735, -93.119224
+        centerOfNeighbor.push([44.931735, -93.119224]);
+        L.marker([44.931735, -93.119224]).addTo(mymap).bindPopup(commited[8]+' crimes commited').openPopup();
+        // 10|Como; 44.982860, -93.150844
+        centerOfNeighbor.push([44.982860, -93.150844]);
+        L.marker([44.982860, -93.150844]).addTo(mymap).bindPopup(commited[9]+' crimes commited').openPopup();
+        // 11|Hamline/Midway; 44.962891, -93.167436
+        centerOfNeighbor.push([44.962891, -93.167436]);
+        L.marker([44.962891, -93.167436]).addTo(mymap).bindPopup(commited[10]+' crimes commited').openPopup();
+        // 12|St. Anthony; 44.973546, -93.195991
+        centerOfNeighbor.push([44.973546, -93.195991]);
+        L.marker([44.973546, -93.195991]).addTo(mymap).bindPopup(commited[11]+' crimes commited').openPopup();
+        // 13|Union Park; 44.948401, -93.174050
+        centerOfNeighbor.push([44.948401, -93.174050]);
+        L.marker([44.948401, -93.174050]).addTo(mymap).bindPopup(commited[12]+' crimes commited').openPopup();
+        // 14|Macalester-Groveland; 44.934301, -93.175363
+        centerOfNeighbor.push([44.934301, -93.175363]);
+        L.marker([44.934301, -93.175363]).addTo(mymap).bindPopup(commited[13]+' crimes commited').openPopup();
+        // 15|Highland; 44.911489, -93.172075
+        centerOfNeighbor.push([44.911489, -93.172075]);
+        L.marker([44.911489, -93.172075]).addTo(mymap).bindPopup(commited[14]+' crimes commited').openPopup();
+        // 16|Summit Hill; 44.937493, -93.136353
+        centerOfNeighbor.push([44.937493, -93.136353]);
+        L.marker([44.937493, -93.136353]).addTo(mymap).bindPopup(commited[15]+' crimes commited').openPopup();
+        // 17|Capitol River; 44.950459, -93.096462
+        centerOfNeighbor.push([44.950459, -93.096462]);
+        L.marker([44.950459, -93.096462]).addTo(mymap).bindPopup(commited[16]+' crimes commited').openPopup();
+        
+        // latlng in box after pan
+        mymap.on("moveend", function() {
+            
+            tem = {};
+            
+            if (app.addressDiv === true) {
+                var centerLatLng = mymap.getCenter();
+                $.getJSON('https://nominatim.openstreetmap.org/reverse?format=json&lat='+ centerLatLng.lat +'&lon='+centerLatLng.lng+'&zoom=18&addressdetails=1', (data) => {
+                    document.getElementsByName('box2')[0].placeholder = data.address.house_number + ', ' + data.address.road + ', ' + data.address.city;
+                });
+            }
+            else {
+                document.getElementsByName('boxlat')[0].placeholder = 'Lat: ' + mymap.getCenter().lat;
+                document.getElementsByName('boxlng')[0].placeholder = 'Lng: ' + mymap.getCenter().lng;
+            }
+            
+            for (var key in data) {
+                
+                var nei_lat = centerOfNeighbor[data[key].neighborhood_number-1][0];
+                var nei_lng = centerOfNeighbor[data[key].neighborhood_number-1][1];
+                var viwe_bounds = mymap.getBounds();
+                if (insideArea(viwe_bounds, nei_lat, nei_lng)) {
+                    tem[key] = data[key];
+                }
+            }
+            app.stpaulcrimes = tem;
+        });
+    });
+    
+    //$.getJSON('http://cisc-dean.stthomas.edu:8036/codes', (codes)=> {
+    $.getJSON(crime_api_url+'/codes', (codes)=> {
     	app.Codes = codes;
     });
-    $.getJSON('http://localhost:8000/neighborhoods', (neighborhoods)=> {
-    	app.Neighborhoods = neighborhoods;
+    
+    //$.getJSON('http://cisc-dean.stthomas.edu:8036/neighborhoods', (neighborhoods)=> {
+    $.getJSON(crime_api_url+'/neighborhoods', (neighborhoods)=> {
+        app.Neighborhoods = neighborhoods;
     });
-    // https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=-34.44076&lon=-58.70521 latlng to address
-    // https://nominatim.openstreetmap.org/?addressdetails=1&q=bakery+in+berlin+wedding&format=json&limit=1 address to latlng
     
 }
 
-function Search(event) {
-	/*
-	QUERY : https://nominatim.openstreetmap.org/search?<params>
-		street=<housenumber> <streetname>
-		city=<city>
-		county=<county>
-		state=<state>
-		country=<country>
-		postalcode=<postalcode>
-	*/
-	var queryNominatim = 'https://nominatim.openstreetmap.org/search?';
+function insideArea(viewBounds, point_lat, point_lng) {
+    
+    var bound_lat = [viewBounds._southWest.lat, viewBounds._northEast.lat];
+    var bound_lng = [viewBounds._southWest.lng, viewBounds._northEast.lng];
+    
+    if (point_lat > bound_lat[0] && point_lat < bound_lat[1] && point_lng > bound_lng[0] && point_lng < bound_lng[1]) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
 
+function Search(event) {
+	var queryNominatim = 'https://nominatim.openstreetmap.org/search?';
+    
 	if (app.searchType === 'select' || app.searchType === 'address') {
 		var street = app.inputSearch.split(' ');
 		var streetName;
@@ -165,17 +234,206 @@ function Search(event) {
 
 	    $.ajax(request).then(() => {
 	    	latlng = L.latLng(addressLat, addressLng);
-			mymap.flyTo(latlng, 18);
+			//mymap.flyTo(latlng, 18);
+			mymap.setView(latlng, 18);
 	    });
 	}
 
 	if (app.searchType === 'latlong') {
-		if ((app.userLat>=44.888027 && app.userLat<=44.992017) 
-			&& (app.userLng>=-93.208156 && app.userLng<=-93.004975)) {
+		/*if ((app.userLat>=44.888027 && app.userLat<=44.992017) 
+			&& (app.userLng>=-93.208156 && app.userLng<=-93.004975)) {*/
 			latlng = L.latLng(app.userLat, app.userLng);
-			mymap.flyTo(latlng, 18);
+			//mymap.flyTo(latlng, 18);
+
+			mymap.setView(latlng, 18)
+		//}
+	}
+	
+}
+
+function SearchUpper (event) {
+    
+    //var stpaulapi = 'http://cisc-dean.stthomas.edu:8000/incidents?';
+    var stpaulapi = crime_api_url+'/incidents?';
+    var tem2 = {};
+    var tem1 = {};
+    var tem = app.stpaulcrimes;
+    var nei_num = [];
+    
+    if (app.searchStartDate !== '' || app.searchEndDate !== '') {
+        if (app.searchStartDate !== '') {
+            stpaulapi += 'start_date=' + app.searchStartDate + '&';
+        }
+        
+        if (app.searchEndDate !== '') {
+            stpaulapi += 'end_date=' + app.searchEndDate + '&';
+        }
+        
+        $.getJSON(stpaulapi, (newDateData)=> {
+            app.stpaulcrimes = newDateData;
+        });
+    }
+    
+    if (app.searchStartTime !== '' || app.searchEndTime !== '') {
+        
+        var startH = parseInt(app.searchStartTime.split(':')[0]),
+            startM = parseInt(app.searchStartTime.split(':')[1]),
+            endH = parseInt(app.searchEndTime.split(':')[0]),
+            endM = parseInt(app.searchEndTime.split(':')[1]);
+        // 12:00 AM => 0:00 
+        
+        for (var key in app.stpaulcrimes) {
+            
+            var keyH = parseInt(tem[key].time.split(':')[0]),
+                keyM = parseInt(tem[key].time.split(':')[1]);
+
+            if (keyH > startH && keyH < endH) {
+                tem[key] = app.stpaulcrimes[key];
+            } 
+            else if (keyH === startH || keyH === endH) {
+                if (keyM > startM && keyM < endM) {
+                    tem[key] = app.stpaulcrimes[key];
+                }
+                else if (keyM === startM || keyM === endM) {
+                    tem[key] = app.stpaulcrimes[key];
+                }
+            }
+        }
+    }
+    else {
+        tem = app.stpaulcrimes;
+    }
+    
+    for (let k = 0; k < app.neighSelected.length; k++) {
+        nei_num.push(parseInt(app.neighSelected[k], 10));
+    }
+    
+    if (app.crimeSelected.length > 0) {
+        for (var key in tem) {
+            for (let i = 0; i < app.crimeSelected.length; i++) {
+                if (tem[key].incident.includes(app.crimeSelected[i])) {
+                    tem1[key] = tem[key];
+                }
+            }
+        }
+    }
+    else {
+        tem1 = tem;
+    }
+    
+    for (var keys in tem1) {
+        for (let j = 0; j < nei_num.length; j++) {
+            if (parseInt(tem1[keys].neighborhood_number, 10) === nei_num[j]) {
+                tem2[keys] = tem1[keys];
+            }
+        }
+    }
+    
+    app.stpaulcrimes = tem2;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    /*
+	var stpaulapi = 'http://localhost:8000/incidents?';
+	if (app.crimeSelected.length > 0) {
+		stpaulapi += 'code=';
+		for (let i = 0; i < app.crimeSelected.length; i++) {
+			if (i == app.crimeSelected.length-1) { // is last
+				stpaulapi += app.crimeSelected[i] + '&';
+			} else {
+				stpaulapi += app.crimeSelected[i] + ',';
+			}
+		}
+	} 
+
+	if (app.neighSelected.length > 0) {
+		stpaulapi += 'id=';
+		for (let i = 0; i < app.neighSelected.length; i++) {
+			if (i == app.neighSelected.length-1) { // is last
+				stpaulapi += app.neighSelected[i] + '&';
+			} else {
+				stpaulapi += app.neighSelected[i] + ',';
+			}
+		}
+	} 
+
+	if (app.searchStartDate !== '') {
+		stpaulapi += 'start_date='+app.searchStartDate+'&';
+	} 
+
+	if (app.searchEndDate !== '') {
+		stpaulapi += 'end_date='+app.searchEndDate+'&';
+	}
+	console.log(stpaulapi);
+	// Get The Search done !
+	$.getJSON(stpaulapi, (data)=> {   
+        
+        var temporary = {};
+        //console.log(app.searchTime);
+        if (app.searchStartTime !== '' && app.searchEndTime !== '') {
+            // adjusting time
+            startArr =app.searchStartTime.split(':');
+            endArr =app.searchEndTime.split(':');
+            if(startArr.length == 2) {
+                //app.searchStartTime += ':00';
+                app.searchStartTime = startArr[0] + startArr[1] + '00';
+                parseInt(app.searchStartTime);
+            }
+            if(startArr.length == 0) {
+                app.searchStartTime = startArr[0] + '0000';
+                parseInt(app.searchStartTime);
+            }
+            if(endArr.length == 2) {
+                //app.searchStartTime += ':00';
+                app.searchEndTime = endArr[0] + endArr[1] + '00';
+                parseInt(app.searchEndTime);
+            }
+            if(endArr.length == 0) {
+                app.searchEndTime = endArr[0] + '0000';
+                parseInt(app.searchEndTime);
+            }
+
+            for (var key in data) {
+                let timeData = data[key].time.split('.');
+                var ti = timeData[0];
+                if (ti >= app.searchStartTime && ti<=app.searchEndTime) {
+                    temporary[key] = data[key];
+                }
+            }
+            
+            app.stpaulcrimes = temporary;
+        } else {
+            console.log(data);
+            app.stpaulcrimes = data;
+
+        }
+    });
+    */
+}
+
+function getLatLng(address) {
+	var queryNominatim = 'https://nominatim.openstreetmap.org/search?';
+	var street = address.split(' ');
+	var streetName;
+	for (var i = 0; i < street.length; i++) {
+		if (i == 0) {
+			streetName = street[0];
+		} else {
+			streetName = streetName + '%20' + street[i];
 		}
 	}
+
+    $.getJSON(queryNominatim + 'street='+ streetName +'&city=St%20Paul&state=MN&format=json', (data)=> {
+    	addressLat = data[0].lat;
+		addressLng = data[0].lon;
+		return Promise.resolve();
+    });
 	
 }
 
@@ -184,3 +442,32 @@ function setNominatim(data){
 	addressLng = data[0].lon;
 }
 
+function Visualize(event, address, date, time, incident) {
+
+	var queryNominatim = 'https://nominatim.openstreetmap.org/search?';
+	var street = address.split(' ');
+	var streetName;
+
+	if (street[0].includes('X') && !isNaN(street[0].charAt(0))) {
+		street[0] = street[0].replace('X', '0');
+	}
+	
+	for (var i = 0; i < street.length; i++) {
+		if (i == 0) {
+			streetName = street[0];
+		} else {
+			streetName = streetName + '%20' + street[i];
+		}
+	}
+
+    $.getJSON(queryNominatim + 'street='+ streetName +'&city=St%20Paul&state=MN&format=json', (data)=> {
+    	addressLat = data[0].lat;
+		addressLng = data[0].lon;
+		latlng = L.latLng(addressLat, addressLng);
+		mymap.flyTo(latlng, 18); // should it fly to ?
+		var mark = L.marker(latlng).addTo(mymap).bindPopup(address + ', ' + date + ' at ' + time + ',\ntype : ' + incident).openPopup();
+		mark.on('mouseover', function(e) { mymap.removeLayer(mark); });
+    });
+
+
+}
